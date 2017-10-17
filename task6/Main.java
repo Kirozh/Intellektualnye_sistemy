692c16d64b122931f04699bd1772ac3a57158577
Кирилл Ожигин <cuprum.oxigen@gmail.com>
2017-10-18 00:08:28 +0300

    searchinf for letter

--- task6/Main.java
+++ task6/Main.java
@@ -1,59 +1,5 @@
 //package task6;
-/*
-import java.io.*;
-public class Main {
 
-    private final char lookFor;
-    private FileReader file01 = new FileReader("/Users/justin/Desktop/Codes Netbean/JavaRandom/xanadu.txt");
-
-    CountLetter(FileReader file, char lookFor) throws IOException {
-        this.file01 = file;
-        this.lookFor = lookFor;
-
-    }
-
-
-    private String readFile() {
-
-        String everything;
-
-        try (BufferedReader br = new BufferedReader(file01)) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-
-            }
-            everything = sb.toString();
-        }
-        return everything;
-    }
-
-    private int count(String line) {
-        int count = 0;
-
-        for (int i = 0; i < line.length(); i++) {
-            if (line.charAt(i) == lookFor) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    public static void main(String[] args) {
-	// write your code here
-        FileReader file01 = new FileReader("/Users/justin/Desktop/Codes Netbean/JavaRandom/xanadu.txt");
-        CountLetter cl1 = new CountLetter(file01, 'a');
-
-        System.out.println(cl1.count(cl1.readFile()));
-    }
-    }
-}
-*/
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 
@@ -62,9 +8,9 @@ class Main{
     {
     //String s=args[0];
 	System.out.println("Input letter: ");
-    char letter=args[0].charAt(0);
+    	char letter=args[0].charAt(0);
         try{
-        FileInputStream fis=new FileInputStream("C:\\Users\\User\\Temp.txt");
+        FileInputStream fis=new FileInputStream("Temp.txt");
         BufferedInputStream bis=new BufferedInputStream(fis);
         int i;
         int count=0;
@@ -76,7 +22,7 @@ class Main{
                 count++;
             }
         }
-        System.out.println("Letter " + letter +" appears in the file "+count+" TIMES");
+        System.out.println("Letter " + letter +" appears in the file "+count+" times");
     }
     catch(Exception ex)
     {
