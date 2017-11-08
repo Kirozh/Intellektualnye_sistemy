a26921e468e51917805fa18bbd8baec65d772528
Кирилл Ожигин <cuprum.oxigen@gmail.com>
2017-11-08 22:15:45 +0300

    Update Main.java

--- task7/Main.java
+++ task7/Main.java
@@ -1,22 +1,45 @@
-//package task7;
+package task7;
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        final int assumedLineLength = 50;
-        File file = new File(args[0]);
-        List<String> fileList =
-                new ArrayList<String>((int)(file.length() / assumedLineLength) * 2);
-        BufferedReader reader = null;
-        int lineCount = 0;
+
+    public static int lineNumber(File myfile)
+    {
+        int lineNumber = 0;
         try {
-            reader = new BufferedReader(new FileReader(file));
-            for (String line = reader.readLine(); line != null;
-                 line = reader.readLine()) {
-                fileList.add(line);
-                lineCount++;
-            }
+            FileReader filereader = new FileReader(myfile);
+            LineNumberReader linenumberreader = new LineNumberReader(filereader);
+
+
+
+            while (linenumberreader.readLine() != null)
+                lineNumber++;
+
+
+
+            linenumberreader.close();
+        }
+        catch(IOException e ) {
+            System.exit(1);
+        }
+         return lineNumber;
+    }
+
+    public static List<String> randomRepeat(File file, int stringLength) {
+        List<String> lines = new ArrayList<String>((int)(file.length() / stringLength) * 2);
+
+        BufferedReader reader = null;
+
+
+        try {
+            FileReader fReader= new FileReader(file);
+            reader = new BufferedReader(fReader);
+
+            for (String line = reader.readLine(); line != null; line = reader.readLine())
+                lines.add(line);
+
+
         } catch (IOException e) {
             //System.err.println("Could not read file");
             System.exit(1);
@@ -27,11 +50,18 @@ public class Main {
                 } catch (IOException e) {}
             }
         }
-        int repeats = Integer.parseInt(args[1]);
+        return lines;
+    }
+    public static void main(String[] args) {
+        final int stringLength = 50;
+        File myfile = new File(args[0]);
+
+        int i;
+        int numberOfRepeats = Integer.parseInt(args[1]);
         Random random = new Random();
-        for (int i = 0; i < repeats; i++) {
-            System.out.println(i+"\t"+
-                    fileList.get(random.nextInt(lineCount-1)));
+        
+        for (i = 0; i < numberOfRepeats; i++) {
+            System.out.println(" string " + i + " = " + randomRepeat(myfile, stringLength).get(random.nextInt(lineNumber(myfile)-1)));
         }
     }
 }
