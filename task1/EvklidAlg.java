04eddd9d538137728fd112c18ac8952595772127
Кирилл Ожигин <cuprum.oxigen@gmail.com>
2017-12-08 15:04:39 +0300

    Update EvklidAlg.java

--- task1/EvklidAlg.java
+++ task1/EvklidAlg.java
@@ -10,27 +10,49 @@ public class EvklidAlg {
         num2=b;
     }
 
-    public static int UsualEvklidAlg(int num1, int num2)//doesn`t work correct with two negative numbers, zero and negative
-                                                        //for example gcd(2,-1)=-1 gcd(-5,0)=-5
+    public static int UsualEvklidAlg(int num1, int num2)//doesn`t work correct with two negative numbers, 
+                                                        //for example gcd(2,-1)=-1 
     {
-        if (num2 == 0)
+        /*if (num2 == 0)
             return num1;
         else
-            return  UsualEvklidAlg(num2, num1 % num2);
+            return  UsualEvklidAlg(num2, num1 % num2);*/
+        while (num1!=0 && num2!=0) {
+            if (Math.abs(num1) > Math.abs(num2))
+                num1 = num1 % num2;
+            else
+                num2 = num2 % num1;
+        }
+        return num1 + num2;
     }
 
-    public static int EvklidAlgFloorMod(int num1, int num2)
+    public static int EvklidAlgFloorMod(int num1, int num2) //does not work correct with first positive and second negative
     {
-        if (num2 == 0)
+        /*if (num2 == 0)
             return num1;
         else
             return  EvklidAlgFloorMod(num2, Math.floorMod(num1,num2));
+        */
+        while (num1!=0 && num2!=0) {
+            if (Math.abs(num1) > Math.abs(num2))
+                num1 = Math.floorMod(num1 , num2);
+            else
+                num2 = Math.floorMod(num2 , num1);
+        }
+        return num1 + num2;
     }
-    public static int EvklidAlgRem(int num1, int num2)
+    public static int EvklidAlgRem(int num1, int num2) //correct
     {
-        if (num2 == 0)
+     /*   if (num2 == 0)
             return Math.abs(num1);
         else
-            return  EvklidAlgRem(num2, Math.abs(num1)%Math.abs(num2));
+            return  EvklidAlgRem(num2, Math.abs(num1)%Math.abs(num2));*/
+        while (num1!=0 && num2!=0) {
+            if (Math.abs(num1) > Math.abs(num2))
+                num1 = Math.abs(num1 % num2);
+            else
+                num2 = Math.abs(num2 % num1);
+        }
+        return num1 + num2;
     }
 }
